501a27651ebbd518c362008c15f6f96bd5257f12
saikalyan99 <saikalyan.16k@hotmail.com>
2024-11-13 17:54:34 -0600

    Udated parameters

--- project_r/code_modules/search_area.py
+++ project_r/code_modules/search_area.py
@@ -12,18 +12,18 @@ class SearchArea:
         self.radius = radius
         
         # Hexagonal search parameters
-        self.max_radius = 243000                                                                    # Max starting radius in meters
-        self.min_radius = 81000                                                                     # Initial radius for each hexagon in meters
-        self.stage = 1                                                                              # Track stages in hexagonal expansion
-        self.coordinates_list_old = []                                                              # Stores previous stage coordinates 
-        self.coordinates_list = []                                                                  # Store current stage coordinates
-        self.restaurants = []                                                                       # Store found restaurant data
+        self.max_radius = 243000                                                        # Max starting radius in meters
+        self.min_radius = 27000                                                         # Initial radius for each hexagon in meters
+        self.stage = 1                                                                  # Track stages in hexagonal expansion
+        self.coordinates_list_old = []                                                  # Stores previous stage coordinates 
+        self.coordinates_list = []                                                      # Store current stage coordinates
+        self.restaurants = []                                                           # Store found restaurant data
         
         # Correct instantiation of DbDataProcessor with request_type included
         self.data_dump = DbDataProcessor(latitude, longitude, self.haversine_distance, request_type="API")
 
     def grid_initiator(self):
-        """Initiates the grid search process starting with the center hexagon."""
+        """ Initiates the grid search process starting with the center hexagon """
         if self.radius < self.min_radius:
             self.min_radius = self.radius
             self.grid_process_n_check(self.latitude, self.longitude, self.min_radius * 1.1, 0)
@@ -32,17 +32,17 @@ class SearchArea:
             self.grid_calculator(self.latitude, self.longitude, self.min_radius)
 
         self.restaurants = self.data_dump.data_deduplicator(self.restaurants)
-        return self.restaurants                                                                     # Return the aggregated results as a list
+        return self.restaurants                                                         # Return the aggregated results as a list
 
     def grid_calculator(self, center_x, center_y, radius):
-        """Keeps track of position of the hexagons and manages multi-stage expansion."""
+        """ Keeps track of position of the hexagons and manages multi-stage expansion """
         self.grid_process_n_check(center_x, center_y, radius * 1.1, 0)
         
         # Continue expanding in a loop until a kill condition is added
         condition = True
         while condition:
             for i in range((self.stage * 6) + 1):
-                angle = math.radians(60 * i)                                                        # Angle per hexagon in the current ring
+                angle = math.radians(60 * i)                                            # Angle per hexagon in the current ring
                 new_center_x = center_x + (radius * 2 * self.stage) / 111320 * math.cos(angle)
                 new_center_y = center_y + (radius * 2 * self.stage) / (111320 * math.cos(math.radians(center_x))) * math.sin(angle)
                 
@@ -51,14 +51,13 @@ class SearchArea:
                 self.coordinates_list.append((new_center_x, new_center_y))
                 
                 if distance_from_origin > (self.radius + 1.5 * self.min_radius) / 1000:
-                    condition = False                                                               # Break when the radius exceeded requirement
-                    #break
-
+                    condition = False                                                   # Break when the radius exceeded requirement
+                    
                 if i == 0:
                     (temp_x, temp_y) = (new_center_x, new_center_y)
 
                 if i == 6:
-                    # Move to the next stage after processing all six neighbors
+                    """ Move to the next stage after processing all six neighbors """
                     self.coordinates_list_old = self.coordinates_list.copy()
                     self.coordinates_list.clear()
                     self.stage += 1
@@ -66,7 +65,7 @@ class SearchArea:
                     index = 0
                     break
                 else:
-                    # Process each hexagon in the current stage
+                    """ Process each hexagon in the current stage """
                     self.grid_process_n_check(new_center_x, new_center_y, radius * 1.1, 0)
                     if self.stage > 1:
                         for j in range(self.stage - 1):                                 # Generate hexagons in a straight line within the stage
@@ -75,7 +74,7 @@ class SearchArea:
                             self.coordinates_list.append(self.move_external_inline(new_center_x, new_center_y, radius * 1.1, 60 + (60 * i)))
     
     def move_external_inline(self, new_center_x, new_center_y, radius, angle):
-        """Move to the 6 external neighboring hexagons and check them."""
+        """ Move to the 6 external neighboring hexagons and check them """
         rad_angle = math.radians(angle)
         neighbor_center_x = new_center_x + (radius / 111320) * math.cos(rad_angle)
         neighbor_center_y = new_center_y + (radius / (111320 * math.cos(math.radians(new_center_x)))) * math.sin(rad_angle)
@@ -83,21 +82,21 @@ class SearchArea:
         return (neighbor_center_x, neighbor_center_y)
     
     def grid_process_n_check(self, center_x, center_y, radius, depth):
-        """Process and check if internal divide is needed based on restaurant count."""
+        """ Process and check if internal divide is needed based on restaurant count """
         temp_result = self.find_restaurants(center_x, center_y, radius)
         print("API called, no. of restaurants = ", len(temp_result), " depth = ", depth, " , radius =", radius, " at (", center_x, center_y, ")")
-        # If restaurant count exceeds the threshold, proceed to internal division
-        if len(temp_result) > 59:
+
+        if len(temp_result) > 59:                                                       # If restaurant count API limit 59, moves to internal division
             self.internal_divide(center_x, center_y, radius, depth + 1)
         else:
             self.restaurants.extend(temp_result)
 
     def internal_divide(self, center_x, center_y, radius, depth):
-        """Divide the circle into seven smaller circles and gather results."""
+        """ Divide the circle into seven smaller circles and gather results """
         sub_radius = (radius / 3) + (radius / 10)
-        self.grid_process_n_check(center_x, center_y, sub_radius, depth)           # Gather results for central grid first
+        self.grid_process_n_check(center_x, center_y, sub_radius, depth)                # Gather results for central grid first
         
-        for i in range(6):                                                                          # Generate six surrounding hexagon centers
+        for i in range(6):                                                              # Generate six surrounding hexagon centers
             angle = math.radians(60 * i)
             new_lat = center_x + (sub_radius / 111320) * math.cos(angle)
             new_lon = center_y + (sub_radius / (111320 * math.cos(math.radians(center_x)))) * math.sin(angle)
@@ -141,15 +140,13 @@ class SearchArea:
         return unique_restaurants
   
     def haversine_distance(self, lat1, lon1, lat2, lon2):
-        """
-        Calculate the Haversine distance between two points in kilometers.
-        """
-        R = 6371                                                                        # Earth radius in kilometers
+        """ Calculate the Haversine distance between two points in kilometers """
+        earth_radius = 6371                                                             # Earth radius in kilometers
         d_lat = math.radians(lat2 - lat1)
         d_lon = math.radians(lon2 - lon1)
         lat1 = math.radians(lat1)
         lat2 = math.radians(lat2)
 
-        a = math.sin(d_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(d_lon / 2) ** 2
-        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-        return R * c
+        chord_half_sq = math.sin(d_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(d_lon / 2) ** 2
+        angular_distance = 2 * math.atan2(math.sqrt(chord_half_sq), math.sqrt(1 - chord_half_sq))
+        return earth_radius * angular_distance
